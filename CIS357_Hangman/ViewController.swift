5304af19ef993d3a07034e5c69367519e92a9c7c
Johnathon Frocillo <john.frocillo@gmail.com>
2020-03-30 08:43:57 -0400

    Initial Commit

---
+++ CIS357_Hangman/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CIS357_Hangman
+//
+//  Created by Johnathon Frocillo on 3/30/20.
+//  Copyright © 2020 Johnathon Frocillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CIS357_HangmanTests/CIS357_HangmanTests.swift
@@ -0,0 +1,34 @@
+//
+//  CIS357_HangmanTests.swift
+//  CIS357_HangmanTests
+//
+//  Created by Johnathon Frocillo on 3/30/20.
+//  Copyright © 2020 Johnathon Frocillo. All rights reserved.
+//
+
+import XCTest
+@testable import CIS357_Hangman
+
+class CIS357_HangmanTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

48c68a12c7f0e501794a55bec4ef5f92d474413b
Johnathon Frocillo <john.frocillo@gmail.com>
2020-03-30 09:49:25 -0400

    Basic Setup
    The function for changing the image and displaying a losing
    message is complete and currently simply activated by a button.
    In the future it will be activated after a player guesses a letter.

--- CIS357_Hangman/ViewController.swift
+++ CIS357_Hangman/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var stickman: UIImageView!
+    @IBOutlet weak var wrongGuess: UIButton!
+    
+    var stickManNumber = 0;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func clicked_WrongGuess(_ sender: Any) {
+        if (stickManNumber >= 6) {
+            stickman.image = UIImage(named: "stickman-full")
+            
+            // Alert message showing the game is over and lost
+            let alert = UIAlertController(title: "Game Over!", message: "You Lose!!", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: NSLocalizedString("Darn...", comment: "Default action"), style: .default, handler: {_ in
+                self.stickman.image = nil
+            }))
+            self.present(alert, animated: true, completion: nil)
+            
+            // Reset the counter
+            stickManNumber = 0
+            
+            return
+        }
+        else {
+            // Update the image
+            stickman.image = UIImage(named: "stickman-\(stickManNumber+1)")
+            stickManNumber += 1
+        }
+    }
+    
 }
 

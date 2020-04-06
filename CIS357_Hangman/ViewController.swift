d6b0bc8acefbc87b5a2d1944f3e405fc8b53b4c6
Johnathon Frocillo <john.frocillo@gmail.com>
2020-04-06 13:56:01 -0400

    Part 1 complete
    Basic pass and play game is complete.
    Need to use gamecenter and ios GameKit for the rest of the project.
    Achievements, high schores, and possibly online gamecenter play.

--- CIS357_Hangman/ViewController.swift
+++ CIS357_Hangman/ViewController.swift
@@ -13,15 +13,15 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var stickman: UIImageView!
     @IBOutlet weak var wrongGuess: UIButton!
     @IBOutlet weak var guessedWord: UILabel!
-    @IBOutlet weak var guessedLetter: UITextField!
+    @IBOutlet weak var guessedLetterTextField: UITextField!
     
     var stickManNumber = 0
     var chosenWord = ""
-    
+    var guessedLetter:[Character] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.guessedLetter.delegate = self
+        self.guessedLetterTextField.delegate = self
         // Do any additional setup after loading the view.
         let detectTouch = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
         self.view.addGestureRecognizer(detectTouch)
@@ -38,12 +38,89 @@ class ViewController: UIViewController, UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         dismissKeyboard()
         
+        // Get the guessed letter
+        let temp = guessedLetterTextField.text?.uppercased()
+        if temp!.count > 1 {
+            let alert = UIAlertController(title: "Too Many Letters", message: "Only Guess 1 at a Time!", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default ))
+            self.present(alert, animated: true, completion: nil)
+            
+            guessedLetterTextField.text = ""
+            return false
+        }
+        
         // Check if letter already guessed
+        for letter in guessedLetter {
+            if letter == Character(temp!) {
+                let alert = UIAlertController(title: "Already Guessed", message: "You have already guessed that letter!", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default ))
+                self.present(alert, animated: true, completion: nil)
+                
+                guessedLetterTextField.text = ""
+                return false
+            }
+        }
+        
         // Add it to the list of guessed letters
+        guessedLetter.append(Character(temp!))
+        print(guessedLetter)
+        
         // Update the underscores
+        let value = chosenWord.firstIndex(of: Character(temp!))
+        if  value == nil {
+            incorrectGuess()
+        }
+        else {
+            // Correct Guess
+            var word = guessedWord.text!
+            var index: String.Index
+            
+            for x in 0...chosenWord.count-1 {
+                index = chosenWord.index(chosenWord.startIndex, offsetBy: x)
+                // another for loop checking guessword
+                // loop by incrementing by +2 starting at 1
+                if Character(temp!) == chosenWord[index] {
+                    print("success")
+                    
+                    if x == 0 {
+                        word.remove(at: index)
+                        word.insert(Character(temp!), at: index)
+                    }
+                    else {
+                        let removeLetter = word.index(word.startIndex, offsetBy: x*3)
+                        word.remove(at: removeLetter)
+                        word.insert(Character(temp!), at: removeLetter)
+                    }
+                    
+                    guessedWord.text = word
+                    
+                }
+            }
+            
+            // When there's no more underscores, that means the player won. Display message
+            var gameOver = true
+            for x in 0...word.count-1 {
+                index = word.index(word.startIndex, offsetBy: x)
+                if word[index] == "_" {
+                    // There is still an unguessed letter
+                    // The game continues
+                    gameOver = false
+                }
+            }
+            
+            if gameOver {
+                let alert = UIAlertController(title: "Winner!", message: "The Guessing Player has Won!", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: {_ in
+                    self.performSegue(withIdentifier: "unwindToMenu", sender: self)
+                }))
+                self.present(alert, animated: true, completion: nil)
+            }
+            
+            print(word)
+            guessedWord.text = word
+        }
         
-        incorrectGuess()
-        
+        self.guessedLetterTextField.text = ""
         return true
     }
     
@@ -51,6 +128,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
         self.view.endEditing(true)
     }
 
+    
     @IBAction func clicked_WrongGuess(_ sender: Any) {
         incorrectGuess()
     }
--- CIS357_Hangman/WordSelectorViewController.swift
+++ CIS357_Hangman/WordSelectorViewController.swift
@@ -8,39 +8,65 @@
 
 import UIKit
 
-class WordSelectorViewController: UIViewController {
+class WordSelectorViewController: UIViewController, UITextFieldDelegate {
 
+    // ADD ABILITY TO PRESS ENTER AS WELL AS CONTINUE BUTTON
     @IBOutlet weak var chosenWordTextField: UITextField!
+    @IBOutlet weak var continueButton: UIButton!
+    var chosenWord = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.chosenWordTextField.delegate = self
+        let detectTouch = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
+        self.view.addGestureRecognizer(detectTouch)
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
-        // Make sure word is <= 10 letters
+    @objc func dismissKeyboard() {
+        self.view.endEditing(true)
+    }
+    
+    @IBAction func continuePressed(_ sender: UIButton) {
+        // Make sure word is <= 11 letters
         // No whitespace allowed
         // No numbers, no symbols
         
+        chosenWord = chosenWordTextField.text!.uppercased()
+        chosenWordTextField.text = ""
+        
+        if verify(word: chosenWord) {
+            self.performSegue(withIdentifier: "passAndPlaySegue", sender: sender)
+        }
+        else {
+            let alert = UIAlertController(title: "Invalid Word!", message: "You may only enter letters and less than 12 characters", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default ))
+            self.present(alert, animated: true, completion: nil)
+        }
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "passAndPlaySegue" {
             let dest = segue.destination as! ViewController
-            dest.chosenWord = chosenWordTextField.text!
+            dest.chosenWord = chosenWord
             
         }
         
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func verify (word: String) -> Bool {
+        if word.count > 11 {
+            return false
+        }
+        
+        return !word.isEmpty && word.range(of: "[^a-zA-Z]", options: .regularExpression) == nil
+       
     }
-    */
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        dismissKeyboard()
+        continuePressed(continueButton)
+        return true
+    }
 
 }
